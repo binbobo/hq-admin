3376e7681255830fd8df47917522cbf85edb7119
yuhao <i.yu@qq.com>
2017-04-15 01:52:26 +0800

    删除蓝领驿家url

--- src/app/shared/services/url.service.ts
+++ src/app/shared/services/url.service.ts
@@ -1,10 +1,9 @@
 export class Urls {
-    private static readonly debug: boolean = true;//process.env.ENV === "development";
+    private static readonly debug: boolean = false;//process.env.ENV === "development";
     private static readonly base = 'http://api.sinoauto.com/';
-    public static readonly localization = !Urls.debug ? Urls.base.concat('localization/') : "http://localhost:55820/api/";
+    public static readonly localization = !!Urls.debug ? Urls.base.concat('localization/') : "http://localhost:55820/api/";
     public static readonly configuration = !Urls.debug ? Urls.base.concat('zookeeper/') : "http://localhost:8012/api/";
     public static readonly logging = !Urls.debug ? Urls.base.concat('logger/') : 'http://localhost:8013/api/';
-    public static readonly blueCollar = !Urls.debug ? Urls.base.concat('diag/') : 'http://139.217.4.166:5555/diag/';
     public static readonly location = !Urls.debug ? Urls.base.concat('location/') : "http://localhost:8019/api/";
     public static readonly validate = !Urls.debug ? Urls.base.concat('validate/') : "http://localhost:8019/api/";
     public static readonly media = !Urls.debug ? Urls.base.concat('media/') : "http://api.sinoauto.com/media/";
