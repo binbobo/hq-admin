6d4f79ae44068077dd40076d90e4d1bef40ff35a
wufan <875906774@qq.com>
2017-04-15 22:36:35 +0800

    指派工单

--- src/app/pages/chain/assign-order/assign-order.component.ts
+++ src/app/pages/chain/assign-order/assign-order.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assign-order.component.css']
 })
 export class AssignOrderComponent implements OnInit {
-
+  private isShow=false;
+  private isShowPerson=false;
+  // 指派工单表格详情点击事件
+  private OnClickDetail(){
+    this.isShow=true;
+  }
+    // 指派工单表格详情点击后的关闭事件
+  private OnClickClose(){
+    this.isShow=false;
+  }
+  // 指派工单指派以及更改指派人员事件
+ private OnClickPerson(){
+    this.isShowPerson=true;
+  }
+   // 指派工单指派以及更改指派人员关闭事件
+  private OnClickPersonClose(){
+    this.isShowPerson=false;
+  }
   constructor() { }
 
   ngOnInit() {
