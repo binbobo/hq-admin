b806f97a988664cc81960e401d3571b2b13ea750
yuhao <i.yu@qq.com>
2017-04-17 19:32:29 +0800

    添加时间选择控件、、添加常用类库

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PagesRoutingModule, routedComponents } from 'app/pages/pages.routing';
 import { AuthGuard } from 'app/auth/auth.guard';
-import { BsDropdownModule } from 'ngx-bootstrap';
-import { RouterModule } from '@angular/router';
+import { BsDropdownModule, DatepickerModule } from 'ngx-bootstrap';
 import { SharedModule } from 'app/shared/shared.module';
 import { LanguageService } from './locale/language/language.service';
 import { MenuService } from './settings/menu/menu.service';
 import { PagesService } from './pages.service';
+import { NguiDatetimePickerModule } from '@ngui/datetime-picker';
 
 @NgModule({
   imports: [
     PagesRoutingModule,
     SharedModule,
-    RouterModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    NguiDatetimePickerModule
   ],
   providers: [AuthGuard, LanguageService, MenuService, PagesService],
   declarations: [routedComponents]
