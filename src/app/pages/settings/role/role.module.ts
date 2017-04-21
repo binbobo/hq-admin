585302e557ec918472eee5fe3e1757bd4effb7ae
yuhao <i.yu@qq.com>
2017-04-21 19:52:07 +0800

    删除错误代码

--- src/app/pages/settings/role/role.module.ts
+++ src/app/pages/settings/role/role.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScopeListComponent } from './scope-list/scope-list.component';
 import { RoleListComponent } from './role-list/role-list.component';
 import { RoleCreateComponent } from './role-create/role-create.component';
 import { RoleEditComponent } from './role-edit/role-edit.component';
@@ -9,6 +8,6 @@ import { RoleEditComponent } from './role-edit/role-edit.component';
   imports: [
     CommonModule
   ],
-  declarations: [ScopeListComponent, RoleListComponent, RoleCreateComponent, RoleEditComponent]
+  declarations: [RoleListComponent, RoleCreateComponent, RoleEditComponent]
 })
 export class RoleModule { }
