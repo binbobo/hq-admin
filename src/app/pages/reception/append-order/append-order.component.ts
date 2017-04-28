ec3d2c6ff4c5235090668c958b03a98326632dfa
wufan <875906774@qq.com>
2017-04-28 18:17:12 +0800

    增项代码提交

--- src/app/pages/reception/append-order/append-order.component.ts
+++ src/app/pages/reception/append-order/append-order.component.ts
@@ -5,14 +5,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TabsetComponent } from 'ngx-bootstrap';
 import 'rxjs/add/observable/of';
 import { AppendOrderService, AppendOrderSearch, SearchReturnData } from "./append-order.service";
-import { PagedResult } from "app/shared/models";
+import { PagedResult, StorageKeys } from "app/shared/models";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import { LocalDataSource } from "ng2-smart-table";
 import { CustomMaintanceItemEditorComponent } from "app/pages/reception/create-order/custom-maintance-item-editor.component";
-
+import { TypeaheadRequestParams } from "app/shared/directives";
+import * as moment from 'moment';
 @Component({
   selector: 'app-append-order',
   templateUrl: './append-order.component.html',
@@ -21,6 +22,11 @@ import { CustomMaintanceItemEditorComponent } from "app/pages/reception/create-o
 })
 
 export class AppendOrderComponent extends DataList<Order> {
+  otherFee = 0;
+  materialFee = 0;
+  sumFee = 0;
+  workHourFee = 0;
+  isShowAppend = false;
 
   constructor(
     injector: Injector,
@@ -29,16 +35,15 @@ export class AppendOrderComponent extends DataList<Order> {
   ) {
     super(injector, service);
     this.params = new OrderListRequest();
-
     // 搜索模糊查询
 
     this.plateNoData = Observable
       .create((observer: any) => {
         observer.next(this.myForm.controls.state.value);
       })
-      .debounceTime(300)
+      .debounceTime(100)
       .distinctUntilChanged()
-      .mergeMap((token: string) => this.service1.getAppendOrderParma(token));
+      .mergeMap((token: string) => this.service1.getAppendOrderParma(token))
 
     // 根据项目名称获取维修项目
   }
@@ -61,38 +66,106 @@ export class AppendOrderComponent extends DataList<Order> {
   });
   public plateNoData: Observable<SearchReturnData>;
   private listId: string;
-
+  private maintenanceId: any;
   OnSearchClick(e) {
+    this.isShowAppend = true;
     this.SearchappendList = e.item;
     this.listId = (e.item)["id"];
     this.service1.get(this.listId)
       .then(data => {
         console.log(data);
+        this.maintenanceId = data.id;
+
         //维修项目
         this.maintenanceProjectData = data.serviceOutputs;
+        (this.maintenanceProjectData).forEach((item, index) => {          
+          this.workHourFee += item.amount;
+          this.sumFee += this.workHourFee;
+        })
         //附加项目
-        this.attachServiceOutputs = new LocalDataSource(data.attachServiceOutputs);
+        this.attachServiceOutputs = data.attachServiceOutputs;
         // // //建议维修项
-        this.suggestServiceOutputs =new LocalDataSource(data.suggestServiceOutputs);
-        // // // 上次维修记录
-        // this.lastManufactureDetailOutput = data.lastManufactureDetailOutput;
-        // this.lastDataProjectList = data.lastManufactureDetailOutput.serviceOutputs;
-        // this.lastRepairList = data.lastManufactureDetailOutput.productOutputs;
-        // this.lastAddList = data.lastManufactureDetailOutput.attachServiceOutputs;
-        // this.lastSuggestList = data.lastManufactureDetailOutput.suggestServiceOutputs;
+        this.suggestServiceOutputs = data.suggestServiceOutputs;
+        // // 上次维修记录
+
+        this.lastManufactureDetailOutput = data.lastManufactureDetailOutput;
+        this.lastDataProjectList = data.lastManufactureDetailOutput.serviceOutputs;
+        this.lastRepairList = data.lastManufactureDetailOutput.productOutputs;
+        this.lastAddList = data.lastManufactureDetailOutput.attachServiceOutputs;
+        this.lastSuggestList = data.lastManufactureDetailOutput.suggestServiceOutputs;
         // // 客户回访记录
         // this.feedBackInfosOutput = data.feedBackInfosOutput;
-        console.log(this.maintenanceProjectData, this.attachServiceOutputs, this.suggestServiceOutputs)
+
       });
-
-
   }
+
+  // public get typeaheadColumns() {
+  //   return [
+  //     { name: 'plateNo', title: '车牌号' },
+  //     { name: 'id', title: '工单号' }
+  //   ];
+  // }
+
+  // private typeaheadList;
+
+  // public onTypeaheadSelect($event) {
+  //   alert(JSON.stringify($event));
+  // }
+
+  // public get typeaheadSource() {      
+  //   return (params: TypeaheadRequestParams) => {
+  //     this.plateNoData.subscribe(sdata => {
+  //       console.log(sdata);
+  //       this.typeaheadList=sdata;
+  //     let start = (params.pageIndex - 1) * params.pageSize;
+  //     let end = params.pageSize + start;
+  //     let data = this.typeaheadList.filter(m => m.plateNo.includes(params.text)).slice(start, end);
+  //     let total = this.typeaheadList.filter(m => m.plateNo.includes(params.text)).length;
+  //     return Promise.resolve(new PagedResult<any>(data, total, total))
+  //   });
+  //   };
+  // }
+
+
 
   private SearchappendList = {}
 
-  newMaintenanceItemData = [];
+  newMaintenanceItemData = [];  //新增维修项目
+  newAttachData = [];          //新增附加项目
+  newSuggestData=[];        //新增建议维修项目
 
-
+  submitAddOrder() {
+    this.appendOrderSheet();
+    this.appendAttachSheet();
+    this.appendSuggestSheet()
+  }
+  // 提交维修项目
+  appendOrderSheet() {
+    const addData: any = {};
+    addData.maintenanceItems = this.newMaintenanceItemData;
+    console.log(addData.maintenanceItems)
+    this.service1.post(addData).then(() => {
+      console.log('添加维修项目成功');
+    }).catch(err => console.log("添加维修项目失败" + err));
+  }
+  // 提交附加项目
+  appendAttachSheet() {
+    const attachData: any = {};
+    attachData.maintenanceItems = this.newAttachData;
+    console.log(attachData.maintenanceItems)
+    this.service1.post(attachData).then(() => {
+      console.log('添加附加项目成功');
+    }).catch(err => console.log("添加附加项目失败" + err));
+  }
+  // 提交建议维修项
+  appendSuggestSheet() {
+    const suggestData: any = {};
+    suggestData.maintenanceRecommends= this.newSuggestData;
+    console.log(suggestData.maintenanceRecommends)
+    this.service1.suggestpost(suggestData).then(() => {
+      console.log('添加建议维修项成功');
+    }).catch(err => console.log(err));
+  }
   /**
  * 通过智能表格新增维修项目, 添加按钮点击事件处理程序
  * @memberOf CreateOrderComponent
@@ -100,34 +173,127 @@ export class AppendOrderComponent extends DataList<Order> {
   addNewmaintanceItem(evt) {
     // 获取新增的维修工项记录
     const newData = evt.newData;
-    // 判断数据的合法性
-    const isValid = true;
 
-    if (isValid) {
-      // 数据合法, 允许添加
-      const confirm = evt.confirm;
-      confirm.resolve();
+    // 报错错我信息
+    let errorMsg = '';
 
-      // 保存新增的维修项目
-      this.newMaintenanceItemData.push({
-        serviceName: newData.serviceName,
-        workHour: newData.serviceName,
-        price: newData.money,
-        discount: newData.money,
-      });
-    };
-    console.log(this.newMaintenanceItemData)
+    // 检查维修项目合法性
+    if (!newData.serviceName) {
+      errorMsg = '请选择一个维修项目';
+      return;
+    }
+
+    // 浮点数正则表达式
+    const reg = /^(?=.+)(?:[1-9]\d*|0)?(?:\.\d+)?$/;
+    // 检查工时合法性
+    if (!newData.workHour || !reg.test(newData.workHour)) {
+      errorMsg = '工时只能输入数字';
+      return;
+    }
+    // 检查单价合法性
+    if (!newData.price || !reg.test(newData.price)) {
+      errorMsg = '单价只能输入数字';
+      return;
+    }
+    // 检查折扣率合法性
+    if (!newData.discount || !reg.test(newData.discount)) {
+      errorMsg = '折扣率只能输入数字';
+      return;
+    }
+
+    // 处理维修项目数据
+    newData.serviceId = sessionStorage.getItem(StorageKeys.MaintanceItemId); // 维修项目id
+    newData.maintenanceId = this.maintenanceId;
+    console.log(this.maintenanceId)
+    newData.amount = newData.workHour * newData.price * (newData.discount / 100); // 金额 = 工时*单价*折扣率
+    newData.operationTime = moment().format('YYYY-MM-DD hh:mm:ss'); // 默认为当前时间
+    newData.type = '3';  // 类型 3表示维修新增
+    // 添加维修项目
+    this.newMaintenanceItemData.push(newData);
+    // 计算工时费
+    this.workHourFee += newData.money;
+    this.sumFee = this.workHourFee + this.materialFee + this.otherFee;
+    // 数据合法, 允许添加
+    evt.confirm.resolve(newData);
   }
 
-  // 维修项目 
+  /**
+    * 通过智能表格删除维修项目,事件处理程序
+    * @memberOf CreateOrderComponent
+    */
+  deleteMaintanceItem(evt) {
+    // 确认删除
+    evt.confirm.resolve();
+
+    // 移除维修项目
+    this.newMaintenanceItemData.forEach((item, index) => {
+      if (evt.data.serviceName === item.serviceName) {
+        // 将新增的维修项目从本地内存中移除
+        this.newMaintenanceItemData.splice(index, 1);
+        return;
+      }
+    });
+  }
+
+  /**
+  * 通过智能表格新增维修附加项目,
+  * @memberOf CreateOrderComponent
+  */
+  addAttachItem(evt) {
+
+    const newData = evt.newData;
+    if (!newData.description) {
+      return;
+    }   
+    newData.maintenanceId = this.maintenanceId; //工单id
+    newData.serviceId = sessionStorage.getItem(StorageKeys.MaintanceItemId); // 维修项目id
+    newData.type = '2';  // 类型2表示附加项目
+    console.log(newData)
+    // 添加维修项目
+    this.newAttachData.push(newData);
+    // 数据合法, 允许添加
+    evt.confirm.resolve(newData);
+  }
+  /**
+  * 通过智能表格删除附加项目
+  * @memberOf CreateOrderComponent
+  */
+  deleteAttachItem(evt) {
+    // 确认删除
+    evt.confirm.resolve();
+  }
+
+  /**
+  * 通过智能表格新增建议维修项目,
+  * @memberOf CreateOrderComponent
+  */
+  suggestItem(evt) {
+    const newData = evt.newData;
+    if (!newData.content) {
+      return;
+    }
+    newData.operateDateTime = moment().format('YYYY-MM-DD hh:mm:ss'); // 默认为当前时间
+    newData.maintenanceId = this.maintenanceId; //工单id
+    console.log(newData)
+    this.newSuggestData = newData;
+    // 数据合法, 允许添加
+    evt.confirm.resolve(newData);
+  }
+  /**
+  * 通过智能表格删除建议维修项目
+  * @memberOf CreateOrderComponent
+  */
+  deletsuggestItem(evt) {
+    // 确认删除
+    evt.confirm.resolve();
+  }
+
+  // 智能表格配置维修项目 
   maintanceItemSettings = {
-    actions: {
-      delete: false, // 不显示删除按钮
-      edit: false,   // 不显示编辑按钮
-    },
     columns: {
       serviceName: {
         title: "维修项目名称",
+        filter: false,
         type: 'html',
         editor: {
           type: 'custom',
@@ -136,18 +302,23 @@ export class AppendOrderComponent extends DataList<Order> {
       },
       workHour: {
         title: '维修工时(小时)',
+        filter: false
       },
       price: {
-        title: '工时单价(元)'
+        title: '工时单价(元)',
+        filter: false
       },
-      money: {
+      amount: {
         editable: false,
-        title: '金额(元)'  // 需要自己计算：工时 * 单价, 不需要传给后台
+        title: '金额(元)',  // 需要自己计算：工时 * 单价, 不需要传给后台
+        filter: false
       },
       discount: {
-        title: '折扣率(%)'
+        title: '折扣率(%)',
+        filter: false
       },
       operationTime: {
+        filter: false,
         editable: false,
         title: '操作时间', // 不需要传给后台
       }
@@ -156,7 +327,8 @@ export class AppendOrderComponent extends DataList<Order> {
   //附加项目
   addItemSettings = {
     columns: {
-      note: {
+      description: {
+        filter: false,
         title: '备注'
       }
     }
@@ -165,14 +337,22 @@ export class AppendOrderComponent extends DataList<Order> {
   // 建议维修项
   suggestedMaintanceSettings = {
     columns: {
-      suggest: {
-        title: '建议维修项目'
+      content: {
+        title: '建议维修项目',
+        filter: false,
+        editor: {
+          type: 'custom',
+          component: CustomMaintanceItemEditorComponent
+        },
       },
-      time: {
-        title: "操作时间"
+      operateDateTime: {
+        editable: false,
+        title: "操作时间",
+        filter: false
       },
-      note: {
-        title: "备注"
+      description: {
+        title: "备注",
+        filter: false
       }
     }
   }
--- src/app/pages/reception/append-order/append-order.service.ts
+++ src/app/pages/reception/append-order/append-order.service.ts
@@ -15,7 +15,7 @@ export class AppendOrderService {
   // }
 
   //  根据工单或者车牌号搜索
-  getAppendOrderParma(keyword: string): Observable<SearchReturnData[]> {
+  getAppendOrderParma(keyword?: string): Observable<SearchReturnData[]> {
     const url = Urls.chain.concat('/Maintenances/increase/search');
     return this.httpService
       .request(url, {
@@ -23,37 +23,41 @@ export class AppendOrderService {
           'Keyword': keyword
         }
       })
-      .map(response => {
+      .map(response => {    
         return response.json().data as SearchReturnData[];
+
       });
   }
+
 
   // 根据id查询(维修项目、附加项目、建议维修项)
   public get(id: string): Promise<DetailData> {
     const url = Urls.chain.concat('/Maintenances/', id);
     return this.httpService
       .get<ApiResult<DetailData>>(url)
-      .then(result => result.data)
+      .then(result =>{ 
+        return result.data})
       .then(data => data || Promise.reject('获取数据无效！'))
       .catch(err => Promise.reject(`加载失败：${err}`));
   }
 
   // 新增维修项目及附加项目
-  public post(body: MainItems): Promise<MainItems> {
-    const url = Urls.chain.concat('/MaintenanceItemOutput/');
+  public post(body:any): Promise<any> {
+    const url = Urls.chain.concat('/MaintenanceItems/list');
     return this.httpService
-      .post<ApiResult<MainItems>>(url, body)
-      .then(result => result.data)
-      .catch(err => Promise.reject(`添加增项失败：${err}`));
+      .post<ApiResult<any>>(url, body)
+      .catch(err => Promise.reject(`${err}`));
   }
 
 
   // 新增建议维修项目
-
-
-
+  public suggestpost(body: any): Promise<any> {
+    const url = Urls.chain.concat('/MaintenanceRecommends/list');
+    return this.httpService
+      .post<ApiResult<any>>(url, body)
+      .catch(err => Promise.reject(`${err}`));
+  }
 }
-
 
 export class AppendOrderSearch extends PagedParams {
   public Keyword: string;
@@ -90,28 +94,12 @@ export class SearchReturnData {
 
 export class DetailData {
   constructor(
+    public id:any,
     public serviceOutputs: any = [],//维修项目
     public attachServiceOutputs: Array<any> = [],//附加项目
     public suggestServiceOutputs: any = [],//建议维修项
     public lastManufactureDetailOutput: any = [],//上次维修记录
     public feedBackInfosOutput: any = [],// 客户回访记录
-
-  ) { }
-}
-
-//提交维修项目及附加项目
-export class MainItems {
-  constructor(
-    public id: string,
-    public maintenanceId: string, //维修工单（Maintenances表-Id字段） ,
-    public itemId: string,//关联维修工项Id 返工单需要此字段 ,
-    public serviceId: string,//维修项目（Services表-Id字段） ,
-    public discount: any,// 折扣率 ,
-    public serviceName: string,// 附加或维修项目名称 ,
-    public type: string,//明细类型 1：维修项目2：附加项目,
-    public workHour: any,//工时（维修项目） ,
-    public price: any,// 金额（工时费或附加产品金额） 单位分 ,
-    public amount: any, //工时*单价=总金额 单位分 ,
   ) { }
 }
 
