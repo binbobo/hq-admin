d9b64a123f49934ff49a7bf8a019ede15900a084
wangbin <2249441872@qq.com>
2017-05-03 13:28:29 +0800

    解决两个菜单公用一个组件的问题

--- src/app/pages/chain/workshop/workshop.routing.ts
+++ src/app/pages/chain/workshop/workshop.routing.ts
@@ -3,16 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { MaintenanceCheckComponent } from './check/maintenance-check.component';
 
 
-const routes: Routes = [{
-    // 维修派工路由
-    path: '',
-    children: [
-        // 维修验收路由
-        { path: 'check', component: MaintenanceCheckComponent },
-        { path: 'assign', redirectTo: '/chain/reception/assign' },
-    ]
-
-}];
+const routes: Routes = [
+    { path: 'check', component: MaintenanceCheckComponent },
+    { path: 'assign', redirectTo: '/chain/reception/assign' }
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
