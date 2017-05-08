b8498d6c68f8961dcc92fe4dbd8b7d14f37c7f91
jiangyuanlong <jiangyl@sinoauto.com>
2017-05-08 15:24:31 +0800

    用户界面

--- src/app/pages/chain/system/usercenter/usercenter.service.ts
+++ src/app/pages/chain/system/usercenter/usercenter.service.ts
@@ -1,6 +1,49 @@
 import {Injectable} from "@angular/core"
+import { Urls, HttpService } from "app/shared/services";
+import { PagedParams, PagedResult, ApiResult, BasicModel, BasicService, ListResult, SelectOption } from 'app/shared/models';
 
 @Injectable()
-export class UserCenterService{
-    
+export class UserCenterService implements BasicService<any>{
+    constructor(
+        private httpService: HttpService
+    ){
+
+    }
+
+    create(body: any): Promise<any> {
+        throw new Error('Method not implemented.');
+    }
+    update(body: any): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    patch(body: any): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+    delete(id: string): Promise<void>{
+         throw new Error('Method not implemented.');
+    }
+
+    getPagedList(params: PagedParams): Promise<PagedResult<UserModel>>{
+        let model:Array<UserModel>=[
+            new UserModel(),
+            new UserModel(),
+            new UserModel()
+        ];
+        
+         return new Promise(null);
+    }
+    get(id: string): Promise<UserModel>{
+         throw new Error('Method not implemented.');
+    }
+}
+
+class UserModel{
+    id :string="1001";
+    account:string="kkis";
+    name:string;
+    avatar:string;
+    mobile:string;
+    email:string;
+    roleIds:string[];
+    tenantIds :string;
 }
