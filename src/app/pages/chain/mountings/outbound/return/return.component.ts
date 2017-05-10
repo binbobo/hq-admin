0675564341202ec686e85af53ea69deaa2bf2d46
yuhao <i.yu@qq.com>
2017-05-10 14:00:56 +0800

    优化表格搜索组件，修改分转元通道

--- src/app/pages/chain/mountings/outbound/return/return.component.ts
+++ src/app/pages/chain/mountings/outbound/return/return.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TypeaheadRequestParams } from 'app/shared/directives';
+import { ProviderService, ProviderListRequest } from '../../provider/provider.service';
 
 @Component({
   selector: 'hq-return',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReturnComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private providerService: ProviderService
+  ) { }
 
   ngOnInit() {
+  }
+
+  public get columns() {
+    return [
+      { name: 'code', title: '代码' },
+      { name: 'name', title: '名称', weight: 1 },
+      { name: 'contactUser', title: '联系人' },
+    ];
+  }
+
+  public onProviderSelect(event) {
+    
+  }
+
+  public get source() {
+    return (params: TypeaheadRequestParams) => {
+      let p = new ProviderListRequest(params.text, params.text);
+      p.setPage(params.pageIndex, params.pageSize);
+      return this.providerService.getPagedList(p);
+    };
   }
 
 }
