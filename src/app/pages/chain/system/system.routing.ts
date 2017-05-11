8af951324506422dad9701bed58e3cdac3343a81
yuhao <i.yu@qq.com>
2017-05-11 19:27:11 +0800

    添加角色管理结构

--- src/app/pages/chain/system/system.routing.ts
+++ src/app/pages/chain/system/system.routing.ts
@@ -4,8 +4,8 @@ import { UsercenterComponent } from "./usercenter/usercenter.component"
 const routes: Routes = [
     { path: "usercenter", component: UsercenterComponent },
     { path: 'accessories', loadChildren: 'app/pages/chain/system/accessories/accessories.module#AccessoriesModule' },
+    { path: 'user-center', loadChildren: 'app/pages/chain/system/user-center/user-center.module#UserCenterModule' },
 ];
-
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
---
+++ src/app/pages/chain/system/user-center/role/role-create/role-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hq-role-create',
+  templateUrl: './role-create.component.html',
+  styleUrls: ['./role-create.component.css']
+})
+export class RoleCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
