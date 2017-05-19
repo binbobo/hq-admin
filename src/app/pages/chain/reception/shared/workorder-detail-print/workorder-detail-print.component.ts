79f324fcbee6d0b02655a752943a928511e972b5
wangbin <2249441872@qq.com>
2017-05-19 13:48:23 +0800

    修复新增维修项目费用统计bug

--- src/app/pages/chain/reception/shared/workorder-detail-print/workorder-detail-print.component.ts
+++ src/app/pages/chain/reception/shared/workorder-detail-print/workorder-detail-print.component.ts
@@ -9,6 +9,9 @@ export class WorkorderDetailPrintComponent implements OnInit {
   @Input()
   data: any;
 
+  tableCaption: Array<string> = ['表一: 维修项目', '表二: 维修配件', '表三: 附加项目', '表四: 建议维修项目'];
+  currentIndex = 0;
+
   constructor() { }
 
   ngOnInit() {
