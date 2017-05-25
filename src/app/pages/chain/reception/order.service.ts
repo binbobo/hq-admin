dde51d2a194e1865ae140a97bb60149669f74128
wangbin <2249441872@qq.com>
2017-05-25 14:18:55 +0800

    查询范围数据组织

--- src/app/pages/chain/reception/order.service.ts
+++ src/app/pages/chain/reception/order.service.ts
@@ -145,6 +145,7 @@ export class OrderService implements BasicService<Order> {
                 let orgs = null;
                 if (this.orgsRecursion([response.json().data])) {
                     orgs = <any>this.orgsRecursion([response.json().data])[0];
+                    console.log('test:', orgs)
                 }
                 // 每个车主下面可能有多个车辆信息
                 return [new TreeviewItem(orgs)];
@@ -157,10 +158,19 @@ export class OrderService implements BasicService<Order> {
             return null;
         };
         return orgsArr.map((value, index, array) => {
-            const obj = { text: value.name, value: value.id };
+            // 导航节点
+            const obj = { text: value.name, value: null };
             // 如果有子组织, 递归遍历
             if (value.children && value.children.length > 0) {
+                 // 如果有孩子  将父节点组织到孩子节点中  放到前头
+                value.children = [{
+                    name: value.name + '总店',
+                    id: value.id
+                }].concat(value.children);
                 obj['children'] = this.orgsRecursion(value.children);
+            } else {
+                // 如果没有孩子  直接返回
+                obj.value = value.id;
             }
             return obj;
         });
