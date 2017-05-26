95d124635095086d77dea7ecd733e8f872e2a3b0
yuhao <i.yu@qq.com>
2017-05-26 19:03:12 +0800

    修改bug

---
+++ src/app/pages/chain/chain-shared/table-typeahead/product-brand-typeahead.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ProductBrandTypeaheadDirective } from './product-brand-typeahead.directive';
+
+describe('ProductBrandTypeaheadDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ProductBrandTypeaheadDirective();
+    expect(directive).toBeTruthy();
+  });
+});
