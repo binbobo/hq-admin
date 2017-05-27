e3bee936b436554b4545986ba021bdc6b4822dd1
yuhao <i.yu@qq.com>
2017-05-27 13:46:53 +0800

    调整配件流水账表格顺序

--- src/app/pages/chain/mountings/inventory/inventory-create/inventory-create.component.ts
+++ src/app/pages/chain/mountings/inventory/inventory-create/inventory-create.component.ts
@@ -62,6 +62,7 @@ export class InventoryCreateComponent extends FormHandle<Inventory> implements O
       packageInfo: [this.model.packageInfo, [Validators.maxLength(20)]],
       madeIn: [this.model.madeIn, [Validators.maxLength(100)]],
       description: [this.model.description, [Validators.maxLength(200)]],
+      productId: ['']
     });
   }
 
@@ -153,6 +154,7 @@ export class InventoryCreateComponent extends FormHandle<Inventory> implements O
       unit: event.unitId,
       categoryId: event.categoryId,
       categoryName: event.categoryName,
+      productId: event.id,
     };
     this.form.patchValue(item);
     this.vehicles = event.vehicleInfoList;
@@ -173,7 +175,7 @@ export class InventoryCreateComponent extends FormHandle<Inventory> implements O
   }
 
   private disableItem(disabled: boolean, ...sources: Array<string>) {
-    let keys = ['packageInfo', 'madeIn', 'brandName'];
+    let keys = ['packageInfo', 'madeIn', 'brandName', 'productId'];
     keys.push.apply(keys, sources.filter(m => m));
     if (disabled) {
       this.owned = true;
