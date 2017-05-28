9936afcd137f81c0430df9870bb363dc1507985c
yuhao <i.yu@qq.com>
2017-05-28 18:14:54 +0800

    添加打印统一头

--- src/app/shared/directives/print/print.directive.ts
+++ src/app/shared/directives/print/print.directive.ts
@@ -1,11 +1,20 @@
-import { Directive, ElementRef, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
+import { Directive, ElementRef, ViewContainerRef, ComponentFactoryResolver, OnInit, Input } from '@angular/core';
 import { PrintComponent } from './print.component';
+
+declare var jQuery: any;
+
 
 @Directive({
   selector: '[hqPrint]',
   exportAs: 'hq-print',
 })
-export class PrintDirective {
+export class PrintDirective implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+  @Input('print-title')
+  private title: string;
 
   constructor(
     private el: ElementRef,
@@ -23,15 +32,15 @@ export class PrintDirective {
     let hiddenItems = copy.querySelectorAll('.print-visible');
     Array.from(hiddenItems).forEach(dom => dom.classList.remove('print-visible'));
     componentRef.instance.html = copy.outerHTML;
+    componentRef.instance.title = this.title;
     this.hideOthers();
     document.body.insertAdjacentElement('afterbegin', componentRef.location.nativeElement);
     setTimeout(() => {
       window.print();
-      this.showOthers();
-      componentRef.location.nativeElement.remove();
-      copy = null;
+      // this.showOthers();
+      // componentRef.location.nativeElement.remove();
+      // copy = null;
     }, 100);
-
   }
 
   private hideOthers() {
