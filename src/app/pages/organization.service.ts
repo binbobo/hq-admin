c0b45fd141480a7d43bfc8cbc16f3fbbfcb0a159
yuhao <i.yu@qq.com>
2017-05-31 20:13:43 +0800

    添加组织和员工服务

---
+++ src/app/pages/organization.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpService, UserService } from 'app/shared/services';
+import { Urls } from '../shared/services/url.service';
+import { StorageKeys } from 'app/shared/models';
+
+@Injectable()
+export class OrganizationService {
+    constructor(
+        private httpService: HttpService,
+        private userService: UserService
+    ) {
+        this.userService.onUserLogout.subscribe(() => this.clearStorage());
+    }
+
+    private clearStorage() {
+        sessionStorage.removeItem(StorageKeys.OrganizationInfo);
+    }
+
+    public getOrganization(): Promise<OrganizationInfo> {
+        let cache = this.getFromStorage();
+        if (cache) return Promise.resolve(cache);
+        return this.getFromApi()
+            .then(data => {
+                sessionStorage.setItem(StorageKeys.OrganizationInfo, JSON.stringify(data));
+                return data;
+            });
+    }
+
+    private getFromStorage() {
+        let key = StorageKeys.OrganizationInfo;
+        let cache = sessionStorage.getItem(key);
+        if (cache) {
+            try {
+                return JSON.parse(cache) as OrganizationInfo;
+            } catch (error) {
+                this.clearStorage();
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    private getFromApi() {
+        let url = Urls.platform.concat('/OrganizationInfos/Current');
+        return this.httpService.get<OrganizationInfo>(url);
+    }
+
+}
+
+export class OrganizationInfo {
+    public id: string;
+    public name: string;
+    public code: string;
+    public address: string;
+    public contactUser: string;
+    public contactInfo: string;
+    public areaId: string;
+    public serviceCall: string;
+    public rescueCall: string;
+}
