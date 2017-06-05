b01b4c405a54549d05e9f4155432bdd6ff2e0603
spring <jianjun6668@163.com>
2017-06-05 15:51:49 +0800

    修改打印样式

--- src/app/pages/chain/report/mountings/statistics/procurement/procurement-print/procurement-print.component.ts
+++ src/app/pages/chain/report/mountings/statistics/procurement/procurement-print/procurement-print.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CentToYuanPipe, DurationHumanizePipe } from "app/shared/pipes";
+import { DataList } from "app/shared/models";
+import { TreeviewItem, TreeviewConfig } from "ngx-treeview/lib";
 
 @Component({
   selector: 'hq-procurement-print',
@@ -6,7 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./procurement-print.component.css']
 })
 export class ProcurementPrintComponent implements OnInit {
-
+  public newDate=this.getDate();
   constructor() { }
 
   @Input() detail
@@ -15,5 +18,17 @@ export class ProcurementPrintComponent implements OnInit {
   
   ngOnInit() {
   }
-
+  toDou(n){
+    return n>9?''+n:'0'+n;
+  }
+  getDate(){
+    let date=new Date();
+    let y=date.getFullYear();
+    let m=date.getMonth()+1;
+    let d=date.getDate();
+    let h=date.getHours();
+    let mm=date.getMinutes();
+    let s=date.getSeconds();
+    return y+'-'+this.toDou(m)+'-'+this.toDou(d)+' '+this.toDou(h)+':'+this.toDou(mm)+':'+this.toDou(s)
+  }
 }
