fac41ad7c354166b5b8e2beca39aa18d7e0a07d4
spring <jianjun6668@163.com>
2017-06-09 13:59:05 +0800

    添加表单输入大小写转换指令

---
+++ src/app/shared/directives/conversion-case/conversion-case.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ConversionCaseDirective } from './conversion-case.directive';
+
+describe('ConversionCaseDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ConversionCaseDirective();
+    expect(directive).toBeTruthy();
+  });
+});
