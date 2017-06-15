e988da2831ec138905a0d69d1e431b805aef89dd
yuhao <i.yu@qq.com>
2017-06-15 11:36:15 +0800

    修改表格内容改变时表头错乱的问题，调整菜单切换时现在加载效果

--- src/app/shared/directives/header-fixed/header-fixed.directive.ts
+++ src/app/shared/directives/header-fixed/header-fixed.directive.ts
@@ -1,44 +1,30 @@
-import { Directive, OnInit, ElementRef } from '@angular/core';
+import { Directive, OnInit, OnDestroy, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Directive({
   selector: '[hqHeaderFixed]'
 })
-export class HeaderFixedDirective implements OnInit {
+export class HeaderFixedDirective implements OnInit, OnDestroy {
 
-  private offsetTop: number;
   private fixed: boolean;
   private thead: HTMLElement;
   private container: HTMLElement;
+  private observer: MutationObserver;
 
-  ngOnInit(): void {
-    this.thead = this.el.nativeElement.querySelector('thead');
-    this.container = this.el.nativeElement.parentElement;
-    if (this.container) {
-      this.offsetTop = this.container.getBoundingClientRect().top;
-      this.container.addEventListener('scroll', (e) => {
-        if (this.container.scrollTop > 0) {
-          this.createCopyHeader();
-        } else {
-          this.cancel();
-        }
-      });
-    }
-  }
-
-  private createCopyHeader() {
+  private fix() {
     if (!this.fixed) {
-      let ths = Array.from(this.thead.querySelectorAll('th'));
-      ths.forEach((th: HTMLTableHeaderCellElement) => {
-        th.width = th.getBoundingClientRect().width + '';
-      });
       let copy = this.thead.cloneNode(true) as HTMLElement;
       copy.classList.add('copy');
       this.thead.insertAdjacentElement('beforebegin', copy);
-      this.thead.style.position = "absolute";
-      ths.forEach((th: HTMLTableHeaderCellElement) => {
+      let headers = Array.from(this.thead.querySelectorAll('th'));
+      headers.forEach((th: HTMLTableHeaderCellElement) => {
+        th.width = th.getBoundingClientRect().width + '';
+      });
+      headers.forEach((th: HTMLTableHeaderCellElement) => {
         th.style.display = "inline-block";
         th.style.borderRightWidth = "0";
       });
+      this.thead.style.position = "absolute";
       this.thead.classList.add('fixed');
     }
     this.thead.style.top = (this.container.scrollTop - 0) + 'px';
@@ -64,5 +50,41 @@ export class HeaderFixedDirective implements OnInit {
   }
 
   constructor(private el: ElementRef) {
+    this.observer = new MutationObserver((mutations) => {
+      if (this.fixed) {
+        this.container.scrollTop = 0;
+        this.cancel();
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    let el = this.el.nativeElement as HTMLElement;
+    let tbody = el.querySelector('tbody');
+    if (!tbody) {
+      console.warn('table中没有包含tbody,无法使用hqHeaderFixed指令');
+      return;
+    }
+    let config = { childList: true };
+    this.observer.observe(tbody, config);
+    this.thead = el.querySelector('thead');
+    if (!this.thead) {
+      console.warn('table中没有包含thead,无法使用hqHeaderFixed指令');
+      return;
+    }
+    this.container = el.parentElement;
+    if (this.container) {
+      this.container.addEventListener('scroll', (e) => {
+        if (this.container.scrollTop > 0) {
+          this.fix();
+        } else {
+          this.cancel();
+        }
+      });
+    }
+  }
+  
+  ngOnDestroy(): void {
+    this.observer && this.observer.disconnect();
   }
 }
