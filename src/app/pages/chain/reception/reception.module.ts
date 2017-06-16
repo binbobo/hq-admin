1226b441d2d579ee9c9fbdf7c90a69b33a079e15
wangbin <2249441872@qq.com>
2017-06-16 13:06:07 +0800

    重新引入treeviewmodule

--- src/app/pages/chain/reception/reception.module.ts
+++ src/app/pages/chain/reception/reception.module.ts
@@ -2,7 +2,6 @@ import {ChainSharedModule} from '../chain-shared/chain-shared.module';
 import { NgModule } from '@angular/core';
 import { ReceptionRoutingModule, routedComponents } from './reception.routing';
 import { DatepickerModule } from 'ngx-bootstrap/datepicker';
-import { TreeviewModule } from 'ngx-treeview';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
@@ -15,6 +14,7 @@ import { NguiDatetimePickerModule } from '@ngui/datetime-picker';
 import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 import { SharedModule } from 'app/shared/shared.module';
 import { BillOrderService } from "app/pages/chain/reception/bill-order/bill-order.service";
+import { TreeviewModule } from 'ngx-treeview';
 
 
 @NgModule({
